ed490a692e1b6c405e3bc6ebb1e8f8f59b9f0e5e
锋 杨 <yangf85@163.com>
2025-03-22 19:51:18 +0800

    修改

--- Cyclone.Wpf/Controls/CopyableTextBlock.cs
+++ Cyclone.Wpf/Controls/CopyableTextBlock.cs
@@ -4,16 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls.Primitives;
 
 namespace Cyclone.Wpf.Controls;
 
-public class CopyableTextBlock:Control
+[TemplatePart(Name = "PART_TextBlock", Type = typeof(TextBlock))]
+public class CopyableTextBlock : Control
 {
+    static CopyableTextBlock()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CopyableTextBlock), new FrameworkPropertyMetadata(typeof(CopyableTextBlock)));
+    }
 
-    #region CopyTextCommand
+    #region Text
 
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(CopyableTextBlock),
+            new FrameworkPropertyMetadata(string.Empty)
+        );
 
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
 
+    #endregion Text
 
-    #endregion
+    #region IsCopyed
+
+    public bool IsCopyed
+    {
+        get => (bool)GetValue(IsCopyedProperty);
+    }
+
+    private static readonly DependencyPropertyKey IsCopyedPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(IsCopyed), typeof(bool), typeof(CopyableTextBlock), new FrameworkPropertyMetadata(false));
+
+    public static readonly DependencyProperty IsCopyedProperty = IsCopyedPropertyKey.DependencyProperty;
+
+    #endregion IsCopyed
+
+    #region Override
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+
+        System.Windows.Clipboard.SetText(Text);
+        SetValue(IsCopyedPropertyKey, true);
+        Focus();
+    }
+
+    protected override void OnLostFocus(RoutedEventArgs e)
+    {
+        base.OnLostFocus(e);
+        SetValue(IsCopyedPropertyKey, false);
+    }
+
+    #endregion Override
 }
