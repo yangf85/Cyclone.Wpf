21adf48d48efaf9aaafc73e161fcc70545e748cb
锋 杨 <yangf85@163.com>
2025-03-24 15:58:38 +0800

    修改

--- Cyclone.Wpf/Helpers/TextBoxHelper.cs
+++ Cyclone.Wpf/Helpers/TextBoxHelper.cs
@@ -17,6 +17,17 @@ public class TextBoxHelper
             new CommandBinding(TextBoxHelper.ClearCommand, OnClear, OnCanClear));
     }
 
+    #region Watermark
+
+    public static string GetWatermark(DependencyObject obj) => (string)obj.GetValue(WatermarkProperty);
+
+    public static void SetWatermark(DependencyObject obj, string value) => obj.SetValue(WatermarkProperty, value);
+
+    public static readonly DependencyProperty WatermarkProperty =
+                DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(default(string)));
+
+    #endregion Watermark
+
     #region HasClearButton
 
     public static readonly DependencyProperty HasClearButtonProperty =
