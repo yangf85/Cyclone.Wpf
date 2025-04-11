643b38ca239a7a0e46ddc181d0736123724d2c37
锋 杨 <yangf85@163.com>
2025-04-11 17:56:21 +0800

    添加 DescriptionBox 和 DescriptionItem 控件及样式
    在 `DescriptionBox.cs` 中定义了 `DescriptionBox` 控件，基于 `ItemsControl`，用于组织 `DescriptionItem` 项目，添加了行间距和列间距的依赖属性，并在 `PrepareContainerForItemOverride` 方法中确保 `DescriptionItem` 在 `Grid` 中的位置。
    
    在 `DescriptionItem.cs` 中定义了 `DescriptionItem` 控件，基于 `ContentControl`，用于显示标签、值和描述，添加了标签、值和描述的依赖属性。
    
    在 `Card - 复制.xaml` 中添加了 `DescriptionBox` 和 `DescriptionItem` 的样式和模板，设置了背景、内边距、行间距、列间距等属性，并定义了其模板。

---
+++ Cyclone.Wpf/Controls/DescriptionBox/DescriptionBox.cs
@@ -0,0 +1,159 @@
+﻿// DescriptionBox.cs
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Cyclone.Wpf.Controls;
+
+/// <summary>
+/// DescriptionBox控件 - 基于ItemsControl的容器，用于组织DescriptionItem项目
+/// </summary>
+[ContentProperty("Items")]
+public class DescriptionBox : ItemsControl
+{
+    static DescriptionBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(DescriptionBox),
+            new FrameworkPropertyMetadata(typeof(DescriptionBox)));
+    }
+
+    public DescriptionBox()
+    {
+        // 设置ItemsPanel为Grid
+        var gridPanel = new ItemsPanelTemplate();
+        var gridFactory = new FrameworkElementFactory(typeof(Grid));
+        gridFactory.SetValue(Grid.IsSharedSizeScopeProperty, true);
+        gridPanel.VisualTree = gridFactory;
+        ItemsPanel = gridPanel;
+    }
+
+    #region 依赖属性
+
+    public static readonly DependencyProperty RowSpacingProperty =
+        DependencyProperty.Register("RowSpacing", typeof(double), typeof(DescriptionBox),
+            new PropertyMetadata(10.0, OnLayoutPropertyChanged));
+
+    public static readonly DependencyProperty ColumnSpacingProperty =
+        DependencyProperty.Register("ColumnSpacing", typeof(double), typeof(DescriptionBox),
+            new PropertyMetadata(10.0, OnLayoutPropertyChanged));
+
+    #endregion 依赖属性
+
+    #region 属性
+
+    /// <summary>
+    /// 行间距
+    /// </summary>
+    public double RowSpacing
+    {
+        get { return (double)GetValue(RowSpacingProperty); }
+        set { SetValue(RowSpacingProperty, value); }
+    }
+
+    /// <summary>
+    /// 列间距
+    /// </summary>
+    public double ColumnSpacing
+    {
+        get { return (double)GetValue(ColumnSpacingProperty); }
+        set { SetValue(ColumnSpacingProperty, value); }
+    }
+
+    #endregion 属性
+
+    private static void OnLayoutPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var box = d as DescriptionBox;
+        var panel = box?.GetItemsPanel() as Grid;
+
+        if (panel != null)
+        {
+            //if (e.Property == RowSpacingProperty)
+            //{
+            //    panel.RowSpacing = (double)e.NewValue;
+            //}
+            //else if (e.Property == ColumnSpacingProperty)
+            //{
+            //    panel.ColumnSpacing = (double)e.NewValue;
+            //}
+        }
+    }
+
+    protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+    {
+        base.PrepareContainerForItemOverride(element, item);
+
+        // 如果容器是DescriptionItem，确保它在Grid中的位置
+        if (element is DescriptionItem descItem && item is DescriptionItem sourceItem)
+        {
+            // 拷贝布局属性
+            Grid.SetRow(descItem, Grid.GetRow(sourceItem));
+            Grid.SetColumn(descItem, Grid.GetColumn(sourceItem));
+            Grid.SetRowSpan(descItem, Grid.GetRowSpan(sourceItem));
+            Grid.SetColumnSpan(descItem, Grid.GetColumnSpan(sourceItem));
+
+            // 确保Grid有足够的行列
+            EnsureGridRowsAndColumns();
+        }
+    }
+
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+        return new DescriptionItem();
+    }
+
+    protected override bool IsItemItsOwnContainerOverride(object item)
+    {
+        return item is DescriptionItem;
+    }
+
+    private void EnsureGridRowsAndColumns()
+    {
+        var panel = GetItemsPanel() as Grid;
+        if (panel == null) return;
+
+        int maxRow = 0;
+        int maxColumn = 0;
+
+        // 找出最大的行和列
+        foreach (var item in Items)
+        {
+            if (item is DescriptionItem descItem)
+            {
+                int row = Grid.GetRow(descItem);
+                int column = Grid.GetColumn(descItem);
+                int rowSpan = Grid.GetRowSpan(descItem);
+                int colSpan = Grid.GetColumnSpan(descItem);
+
+                maxRow = Math.Max(maxRow, row + rowSpan);
+                maxColumn = Math.Max(maxColumn, column + colSpan);
+            }
+        }
+
+        // 确保有足够的行
+        while (panel.RowDefinitions.Count < maxRow)
+        {
+            panel.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+        }
+
+        // 确保有足够的列
+        while (panel.ColumnDefinitions.Count < maxColumn)
+        {
+            panel.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+        }
+    }
+
+    private Panel GetItemsPanel()
+    {
+        if (Template == null)
+            return null;
+
+        var itemsPresenter = Template.FindName("PART_ItemsPresenter", this) as ItemsPresenter;
+        if (itemsPresenter == null)
+            return null;
+
+        return VisualTreeHelper.GetChild(itemsPresenter, 0) as Panel;
+    }
+}
---
+++ Cyclone.Wpf/Controls/DescriptionBox/DescriptionItem.cs
@@ -0,0 +1,73 @@
+﻿// DescriptionBox.cs
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Cyclone.Wpf.Controls;
+
+/// <summary>
+/// DescriptionItem - 显示标签、值和描述的项目
+/// </summary>
+public class DescriptionItem : ContentControl
+{
+    static DescriptionItem()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(DescriptionItem),
+            new FrameworkPropertyMetadata(typeof(DescriptionItem)));
+    }
+
+    public DescriptionItem()
+    {
+        // 设置默认的行列设置
+        Grid.SetRow(this, 0);
+        Grid.SetColumn(this, 0);
+        Grid.SetRowSpan(this, 1);
+        Grid.SetColumnSpan(this, 1);
+    }
+
+    #region 依赖属性
+
+    public static readonly DependencyProperty LabelProperty =
+        DependencyProperty.Register("Label", typeof(string), typeof(DescriptionItem),
+            new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register("Value", typeof(object), typeof(DescriptionItem),
+            new PropertyMetadata(null));
+
+    public static readonly DependencyProperty DescriptionProperty =
+        DependencyProperty.Register("Description", typeof(string), typeof(DescriptionItem),
+            new PropertyMetadata(string.Empty));
+
+    #endregion 依赖属性
+
+    #region 属性
+
+    /// <summary>
+    /// 标签文本
+    /// </summary>
+    public string Label
+    {
+        get { return (string)GetValue(LabelProperty); }
+        set { SetValue(LabelProperty, value); }
+    }
+
+    /// <summary>
+    /// 值对象
+    /// </summary>
+    public object Value
+    {
+        get { return GetValue(ValueProperty); }
+        set { SetValue(ValueProperty, value); }
+    }
+
+    /// <summary>
+    /// 描述文本
+    /// </summary>
+    public string Description
+    {
+        get { return (string)GetValue(DescriptionProperty); }
+        set { SetValue(DescriptionProperty, value); }
+    }
+
+    #endregion 属性
+}
