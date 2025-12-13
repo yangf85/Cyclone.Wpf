357cb8f6a5ad3711b540641546e64dac2619f7d6
锋 杨 <yangf85@163.com>
2025-12-13 20:54:35 +0800

    修改

--- Cyclone.Wpf/Helpers/ScrollViewerHelper.cs
+++ Cyclone.Wpf/Helpers/ScrollViewerHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Specialized;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Cyclone.Wpf.Helpers;
@@ -97,6 +98,121 @@ public static class ScrollViewerHelper
     }
 
     #endregion VerticalOffset 附加属性
+
+    #region IsAutoScrollToEnd 附加属性
+
+    /// <summary>
+    /// IsAutoScrollToEnd 附加属性，用于在集合添加新项时自动滚动到底部
+    /// </summary>
+    public static readonly DependencyProperty IsAutoScrollToEndProperty =
+        DependencyProperty.RegisterAttached(
+            "IsAutoScrollToEnd",
+            typeof(bool),
+            typeof(ScrollViewerHelper),
+            new PropertyMetadata(false, OnIsAutoScrollToEndChanged));
+
+    /// <summary>
+    /// 获取 IsAutoScrollToEnd 附加属性的值
+    /// </summary>
+    /// <param name="obj">依赖对象</param>
+    /// <returns>属性值</returns>
+    public static bool GetIsAutoScrollToEnd(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(IsAutoScrollToEndProperty);
+    }
+
+    /// <summary>
+    /// 设置 IsAutoScrollToEnd 附加属性的值
+    /// </summary>
+    /// <param name="obj">依赖对象</param>
+    /// <param name="value">属性值</param>
+    public static void SetIsAutoScrollToEnd(DependencyObject obj, bool value)
+    {
+        obj.SetValue(IsAutoScrollToEndProperty, value);
+    }
+
+    /// <summary>
+    /// 当 IsAutoScrollToEnd 属性值变化时的回调
+    /// </summary>
+    private static void OnIsAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ScrollViewer scrollViewer)
+            return;
+
+        bool newValue = (bool)e.NewValue;
+
+        if (newValue)
+        {
+            scrollViewer.Loaded += ScrollViewer_Loaded;
+        }
+        else
+        {
+            scrollViewer.Loaded -= ScrollViewer_Loaded;
+            // 清理事件订阅
+            ClearCollectionChangedHandler(scrollViewer);
+        }
+    }
+
+    /// <summary>
+    /// ScrollViewer 加载完成时的处理
+    /// </summary>
+    private static void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not ScrollViewer scrollViewer)
+            return;
+
+        // 查找 ItemsControl
+        if (scrollViewer.Content is ItemsControl itemsControl)
+        {
+            if (itemsControl.ItemsSource is INotifyCollectionChanged collection)
+            {
+                // 先清理旧的事件订阅
+                ClearCollectionChangedHandler(scrollViewer);
+
+                // 创建新的事件处理器
+                NotifyCollectionChangedEventHandler handler = (s, args) =>
+                {
+                    if (args.Action == NotifyCollectionChangedAction.Add)
+                    {
+                        scrollViewer.ScrollToEnd();
+                    }
+                };
+
+                // 保存事件处理器引用，以便后续清理
+                scrollViewer.SetValue(CollectionChangedHandlerProperty, handler);
+
+                // 订阅集合变化事件
+                collection.CollectionChanged += handler;
+            }
+        }
+    }
+
+    // 用于存储事件处理器的私有附加属性
+    private static readonly DependencyProperty CollectionChangedHandlerProperty =
+        DependencyProperty.RegisterAttached(
+            "CollectionChangedHandler",
+            typeof(NotifyCollectionChangedEventHandler),
+            typeof(ScrollViewerHelper),
+            new PropertyMetadata(null));
+
+    /// <summary>
+    /// 清理集合变化事件的订阅
+    /// </summary>
+    private static void ClearCollectionChangedHandler(ScrollViewer scrollViewer)
+    {
+        if (scrollViewer.Content is ItemsControl itemsControl &&
+            itemsControl.ItemsSource is INotifyCollectionChanged collection)
+        {
+            var handler = scrollViewer.GetValue(CollectionChangedHandlerProperty) as NotifyCollectionChangedEventHandler;
+            if (handler != null)
+            {
+                collection.CollectionChanged -= handler;
+                scrollViewer.SetValue(CollectionChangedHandlerProperty, null);
+            }
+        }
+    }
+
+    #endregion IsAutoScrollToEnd 附加属性
 
     #region 辅助方法
 
